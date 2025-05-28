35341e54a62e615c4fd188d4dc660ad034165f86
jonel@DAS-IT <jonel@DAS-IT>
2025-05-29 00:32:19 +0200

    update portfolio_analysis.py  portfolio_analysis.ipynb

--- portfolio_analysis.py
+++ portfolio_analysis.py
@@ -4,10 +4,10 @@ import pandas as pd
 import numpy as np
 
 import matplotlib.pyplot     as plt
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble        import RandomForestClassifier
-from sklearn.metrics         import classification_report
-import joblib
+#from sklearn.model_selection import train_test_split
+#from sklearn.ensemble        import RandomForestClassifier
+#from sklearn.metrics         import classification_report
+#import joblib
 import os
 
 class CreditPortfolioMonitor:
